8eba2a94bfd60b6fb1c0ca68d4d63d42eeb438f2
taeyeonkim-1207 <rlaxodus1207@naver.com>
2022-06-13 16:54:56 +0900

    for문

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -3,5 +3,9 @@ package com.kh.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit");
+		
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+		}
 	}
 }
